859e257a68a05ee090aa48f9fd5dd01eabca9bad
Vuvyan <yuna.vanthart@student.kdg.be>
2023-12-12 10:33:46 +0100

    Create DuplicateRemover2.java

---
+++ collectionsdeel2/src/be/kdg/apps/classes/DuplicateRemover2.java
@@ -0,0 +1,33 @@
+package be.kdg.apps.classes;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+//Hier gebruikt men in plaats van een linkedlist een HashSet
+//een nadeel hier is wel dat een hashset uw volgorde niet bijhoudt zoals je het intypt
+// als je toch de volgorde wilt bijhouden gebruik je een LinkedHashSet
+public class DuplicateRemover2 {
+    public void run() {
+            String woord;
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Geef een aantal woorden, stop met 0");
+            Set<String> woorden = new HashSet<>();
+
+            do {
+                woord = scanner.nextLine();
+                woorden.add(woord); // met hashset worden woorden automatisch geschekt of het er al in zit
+
+
+            } while (!woord.equals("0"));
+            woorden.remove("0");
+            System.out.println("STOP");
+            System.out.println("De woorden zonder duplicaten :");
+            for (String w : woorden) {
+                System.out.println(w);
+            }
+
+        }
+    }
+
