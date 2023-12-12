33cfb999bbd7df6310f389a263f972c598494535
Vuvyan <yuna.vanthart@student.kdg.be>
2023-12-12 10:33:48 +0100

    Create DuplicateRemover.java

---
+++ collectionsdeel2/src/be/kdg/apps/classes/DuplicateRemover.java
@@ -0,0 +1,29 @@
+package be.kdg.apps.classes;
+
+import java.util.*;
+
+//Hier gebruikt men een LinkedList
+public class DuplicateRemover {
+    public void run(){
+        String woord;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Geef een aantal woorden, stop met 0");
+        List<String> woorden = new LinkedList<>();
+
+        do {
+            woord= scanner.nextLine();
+            if (!woorden.contains(woord)){
+                woorden.add(woord);
+            }
+
+        } while (!woord.equals("0"));
+        woorden.remove("0");
+        System.out.println("STOP");
+        System.out.println("De woorden zonder duplicaten :");
+        for (String w:woorden){
+            System.out.println(w);
+        }
+
+    }
+}
