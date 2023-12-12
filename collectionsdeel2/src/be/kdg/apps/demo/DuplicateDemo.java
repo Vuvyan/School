9b849f5f52063feab7febc8322fea7b1b01a9250
Vuvyan <yuna.vanthart@student.kdg.be>
2023-12-12 10:33:35 +0100

    Create DuplicateDemo.java

---
+++ collectionsdeel2/src/be/kdg/apps/demo/DuplicateDemo.java
@@ -0,0 +1,12 @@
+package be.kdg.apps.demo;
+
+import be.kdg.apps.classes.DuplicateRemover;
+
+public class DuplicateDemo {
+    public static void main(String[] args) {
+        //Voor de duplicate remover 1
+        DuplicateRemover dr = new DuplicateRemover();
+        dr.run();
+    }
+}
+
