087f46a219fa0399b2e64861204042b3b2f8612d
Vuvyan <yuna.vanthart@student.kdg.be>
2023-12-12 10:33:44 +0100

    Create DuplicateRemover3.java

---
+++ collectionsdeel2/src/be/kdg/apps/classes/DuplicateRemover3.java
@@ -0,0 +1,32 @@
+package be.kdg.apps.classes;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+//Dit is de versie met de LinkedHashSet
+
+public class DuplicateRemover3 {
+    public void run() {
+        String woord;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Geef een aantal woorden, stop met 0");
+        Set<String> woorden = new LinkedHashSet<>();
+
+        do {
+            woord = scanner.nextLine();
+            woorden.add(woord);
+
+
+        } while (!woord.equals("0"));
+        woorden.remove("0");
+        System.out.println("STOP");
+        System.out.println("De woorden zonder duplicaten :");
+        for (String w : woorden) {
+            System.out.println(w);
+        }
+
+    }
+}
