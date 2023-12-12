ce37dba1b9654c13fac625424635cf3c39e083ef
Vuvyan <yuna.vanthart@student.kdg.be>
2023-12-12 11:49:24 +0100

    Create TextDemo.java

---
+++ collectionsdeel2/src/be/kdg/apps/demo/TextDemo.java
@@ -0,0 +1,10 @@
+package be.kdg.apps.demo;
+
+import be.kdg.apps.classes.TextAnalyser;
+
+public class TextDemo {
+    public static void main(String[] args) {
+        TextAnalyser ta = new TextAnalyser();
+        ta.run();
+    }
+}
