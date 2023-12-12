488d707bc16ec928bc338cec1f3bf3aabda9b1d4
Vuvyan <yuna.vanthart@student.kdg.be>
2023-12-12 11:02:21 +0100

    Update Contacten.java

--- collectionsdeel2/src/be/kdg/apps/classes/Contacten.java
+++ collectionsdeel2/src/be/kdg/apps/classes/Contacten.java
@@ -6,7 +6,7 @@ public class Contacten {
     Scanner scanner = new Scanner(System.in);
     //private List<String> namen = new ArrayList<>();
     //private List<String> telefoonnummers = new ArrayList<>();
-    private Map<String, String> telefoonnummersByName = new HashMap<>();
+    private Map<String, String> telefoonnummersByName = new TreeMap<>();
     public void add(String naam, String telefoonnummer){
        // namen.add(naam);
        // telefoonnummers.add(telefoonnummer);
@@ -15,17 +15,21 @@ public class Contacten {
     }
 
     public void run(){
-        //TODO: geef een naam -> de nummer van jef is [telefoonnummer] OF Jef zit niet in mijn contacten
-        System.out.println("Geef een naam;");
-        String naam = scanner.nextLine();
-        if (telefoonnummersByName.keySet().contains(naam)) {
+        //toon alle namen
+        for (String naam: telefoonnummersByName.keySet()){
+            System.out.println(naam);
+        }
+        // geef een naam -> de nummer van jef is [telefoonnummer] OF Jef zit niet in mijn contacten
+       // System.out.println("Geef een naam;");
+        //String naam = scanner.nextLine();
+        //if (telefoonnummersByName.keySet().contains(naam)) {
             //int index = namen.indexOf(naam);
             //String telefoonnummer = telefoonnummers.get(index);
-            String telefoonnummer = telefoonnummersByName.get(naam);
-            System.out.println("Nummer van " +naam+ ": " + telefoonnummer);
-        } else {
-            System.out.println(naam + " zit niet in mijn contacten...");
-        }
+            //String telefoonnummer = telefoonnummersByName.get(naam);
+            //System.out.println("Nummer van " +naam+ ": " + telefoonnummer);
+       // } else {
+            //System.out.println(naam + " zit niet in mijn contacten...");
+       // }
 
     }
 }
